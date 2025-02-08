ca8ba77badc33ae8ff37fa15250a4ded2808d548
Robert Schuster <robert.r.schuster@deutschebahn.com>
2025-02-08 22:41:50 +0100

    fix: search curtain

--- themes/fiptheme/assets/js/main.js
+++ themes/fiptheme/assets/js/main.js
@@ -2,4 +2,3 @@ import './mobileMenu.js';
 import './countrySelector.js';
 import './resizeObserver.js';
 import './mediaqueries.js';
-import './search.js';
--- themes/fiptheme/assets/js/search.js
+++ themes/fiptheme/assets/js/search.js
@@ -4,12 +4,20 @@ function searchBlur() {
   searchContainer.addEventListener('focusout', function(event) {
     if (searchContainer.contains(event.relatedTarget)) {
       // Der Fokus ist innerhalb des Formulars auf ein anderes Element gesprungen
+      console.log(event.relatedTarget);
+      return;
+    }
+
+    const loadMore = document.querySelector('.pagefind-ui__button');
+
+    if(loadMore) {
+      console.log('Button');
       return;
     }
 
     // Der Fokus ist außerhalb des Formulars gesprungen
     const drawer = document.querySelector('.pagefind-ui__drawer');
-    drawer.classList.add('pagefind-ui__hidden');
+    drawer.classList.add('pagefind-ui__hiddend');
     console.log('Das Formular oder ein Element darin hat den Fokus verloren');
   });
 }
