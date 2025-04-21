94dbd8af552bb527eb41c1ff4fdb4870854d32de
Robert Schuster <robert.r.schuster@deutschebahn.com>
2025-04-21 10:20:53 +0200

    feat: anchorlink

---
+++ assets/js/anchorlink-creator.js
@@ -0,0 +1,39 @@
+function initAnchorlinkCreator() {
+
+  const headings = getHeadings(document.querySelector(".o-single"));
+  const headingsArray = Array.from(headings);
+  console.log(headingsArray);
+
+  headingsArray.forEach((element) => {
+    const anchorLink = createAnchorlink(element.id);
+    element.append(anchorLink);
+    element.insertAdjacentElement('afterend', anchorLink);
+  });
+}
+
+function getHeadings(article) {
+  if (article) {
+     return article.getElementsByTagName("h2");
+  }
+}
+
+function createAnchorlink(id) {
+  const anchor = document.createElement('a');
+  const anchorImg = document.createElement('img');
+  anchor.classList.add('a-anchorlink');
+  anchor.append(anchorImg);
+  anchor.setAttribute('title', 'Link zu diesem Abschnitt kopieren');
+  anchor.setAttribute('href', `#${id}`);
+  anchorImg.setAttribute('src', "/favicon.ico");
+  anchorImg.setAttribute('aria-hidden', "true");
+  return anchor;
+}
+
+
+if (document.readyState === "interactive") {
+  initAnchorlinkCreator();
+} else {
+  window.addEventListener("DOMContentLoaded", () => {
+    initAnchorlinkCreator();
+  });
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,5 @@ import './countrySelector.js';
 import './resizeObserver.js';
 import './mediaqueries.js';
 import './highlightHeadline.js';
+import './anchorlink-creator.js';
 //import './aside.js';
