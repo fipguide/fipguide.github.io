a1e9c16a176f7c4ef97342ec38581a758b6c767f
Robert Schuster <robert.r.schuster@deutschebahn.com>
2025-09-12 18:17:21 +0200

    feat: added current value in mobile content navigation menu

--- assets/js/highlightHeadline.js
+++ assets/js/highlightHeadline.js
@@ -1,11 +1,6 @@
-import * as mq from './mediaqueries';
-
-function isAsideActive() {
-    return window.matchMedia(mq.minLG).matches;
-}
-
 function initHighlightHeadline() {
     const headings = Array.from(document.querySelectorAll('.o-single__highlight :is(h1, h2, h3)'));
+    const mobileTocHeading = document.getElementById('mobileTocHeading');
     const windowPath = window.location.pathname;
     if (headings.length === 0) {
         return;
@@ -23,50 +18,48 @@ function initHighlightHeadline() {
     let height = getDocHeight();
 
     function beginObservation(docHeight) {
-        const observer = new IntersectionObserver(
-            (entries) => {
-				if (!isAsideActive()) {
-					return;
-				}
+      const observer = new IntersectionObserver(
+        (entries) => {
 
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        visibleHeadings.add(entry.target);
-                    } else {
-                        visibleHeadings.delete(entry.target);
-                    }
-                });
+          entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                  visibleHeadings.add(entry.target);
+              } else {
+                  visibleHeadings.delete(entry.target);
+              }
+          });
 
-                // Sort visible (intersecting) headings by inverted order of appearance, then grab the first item (i.e. last visible heading)
-                const lastVisible = Array.from(visibleHeadings.values()).sort((a, b) => headings.indexOf(b) - headings.indexOf(a))[0];
-                if (!lastVisible) {
-                    return;
-                }
+          // Sort visible (intersecting) headings by inverted order of appearance, then grab the first item (i.e. last visible heading)
+          const lastVisible = Array.from(visibleHeadings.values()).sort((a, b) => headings.indexOf(b) - headings.indexOf(a))[0];
+          if (!lastVisible) {
+              return;
+          }
 
-                headings.forEach((heading) => {
-                    // If it's the last visible item, mark it to make it stand out, else, revert to the default style
-                    // Find the link in the TOC list matching the heading in this list of heading elements
-                    const tocLink = getTocLinkFromHeading(heading);
-                    if (heading === lastVisible) {
-                        if(tocLink){
-                            markTocItemActive(tocLink);
-                        }
-                    } else {
-                        if(tocLink){
-                            markTocItemInactive(tocLink);
-                        }
-                    }
-                });
-            },
-            {
-                rootMargin: `${docHeight}px 0px -90% 0px`,
-                threshold: 1, // Only considered intersecting if all the pixels are inside the intersection area
+          headings.forEach((heading) => {
+            // If it's the last visible item, mark it to make it stand out, else, revert to the default style
+            // Find the link in the TOC list matching the heading in this list of heading elements
+            const tocLink = getTocLinkFromHeading(heading);
+            if (heading === lastVisible) {
+              if(tocLink){
+                  markTocItemActive(tocLink);
+                  mobileTocHeading.innerText = tocLink.textContent;
+              }
+            } else {
+              if(tocLink){
+                  markTocItemInactive(tocLink);
+              }
             }
-        );
+          });
+        },
+        {
+            rootMargin: `${docHeight}px 0px -90% 0px`,
+            threshold: 1, // Only considered intersecting if all the pixels are inside the intersection area
+        }
+      );
 
-        headings.forEach((heading) => observer.observe(heading));
+      headings.forEach((heading) => observer.observe(heading));
 
-        return observer;
+      return observer;
     }
 
     currentObserver = beginObservation(height); // Start the intersection observer
