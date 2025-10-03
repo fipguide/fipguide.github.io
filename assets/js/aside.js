21e6c28201d4d836068f09faaf2f0c0d574b618f
Robert Schuster <robert.r.schuster@deutschebahn.com>
2025-10-03 22:10:29 +0200

    prettier

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -5,38 +5,38 @@ function isMobile() {
 }
 
 function initAside() {
-  const bottomSheet = document.querySelector('.o-aside__bottom-sheet');
-  const handleBtn = document.querySelector('.o-aside__bottom-sheet-header');
-  const content = document.querySelector('.o-aside__bottom-sheet-content');
+  const bottomSheet = document.querySelector(".o-aside__bottom-sheet");
+  const handleBtn = document.querySelector(".o-aside__bottom-sheet-header");
+  const content = document.querySelector(".o-aside__bottom-sheet-content");
   const overlay = document.getElementById("overlay");
 
-  let currentState = 'closed'; // 'half', 'full'
+  let currentState = "closed"; // 'half', 'full'
 
   // function lockScroll(lock) {
   //   document.body.style.overflow = lock ? 'hidden' : '';
   // }
 
   function toggleSheet() {
-    if (currentState === 'closed') {
-      bottomSheet.classList.add('open-half');
-      currentState = 'half';
+    if (currentState === "closed") {
+      bottomSheet.classList.add("open-half");
+      currentState = "half";
       overlay.classList.add("overlay--show");
       //lockScroll(true);
-    } else if (currentState === 'half') {
-      bottomSheet.classList.remove('open-half');
-      bottomSheet.classList.add('open-full');
-      currentState = 'full';
+    } else if (currentState === "half") {
+      bottomSheet.classList.remove("open-half");
+      bottomSheet.classList.add("open-full");
+      currentState = "full";
     } else {
-      bottomSheet.classList.remove('open-full');
-      currentState = 'closed';
+      bottomSheet.classList.remove("open-full");
+      currentState = "closed";
       overlay.classList.remove("overlay--show");
       //lockScroll(false);
     }
   }
 
-  handleBtn.addEventListener('click', toggleSheet);
+  handleBtn.addEventListener("click", toggleSheet);
 
-// Drag support
+  // Drag support
   let startY = 0;
   let currentY = 0;
   let isDragging = false;
@@ -45,62 +45,62 @@ function initAside() {
     startY = e.clientY || e.touches?.[0].clientY;
     isDragging = true;
     console.log("dragStart");
-  }
+  };
 
   const dragging = (e) => {
     console.log("isDragging " + isDragging);
     if (!isDragging) return;
-    currentY = (e.clientY || e.touches?.[0].clientY);
-    const deltaY =  startY - currentY;
+    currentY = e.clientY || e.touches?.[0].clientY;
+    const deltaY = startY - currentY;
 
-    if (currentState === 'closed') {
+    if (currentState === "closed") {
       console.log("case closed");
       if (deltaY > 400) {
-        bottomSheet.classList.add('open-full');
-        currentState = 'open-full';
+        bottomSheet.classList.add("open-full");
+        currentState = "open-full";
         overlay.classList.add("overlay--show");
         console.log("set open-full");
       } else if (deltaY > 0) {
-        bottomSheet.classList.add('open-half');
-        currentState = 'open-half';
+        bottomSheet.classList.add("open-half");
+        currentState = "open-half";
         overlay.classList.add("overlay--show");
         console.log("set open-half");
       }
     }
 
-    if (currentState === 'open-half') {
+    if (currentState === "open-half") {
       console.log("case open-half");
       if (deltaY > 0) {
-        bottomSheet.classList.remove('open-half');
-        bottomSheet.classList.add('open-full');
-        currentState = 'open-full';
+        bottomSheet.classList.remove("open-half");
+        bottomSheet.classList.add("open-full");
+        currentState = "open-full";
         console.log("set open-full");
       } else if (deltaY < 0) {
-        bottomSheet.classList.remove('open-half');
-        currentState = 'closed';
+        bottomSheet.classList.remove("open-half");
+        currentState = "closed";
         overlay.classList.remove("overlay--show");
       }
     }
 
-    if (currentState === 'open-full') {
+    if (currentState === "open-full") {
       console.log("case open-full " + deltaY);
       if (deltaY < -400) {
-        bottomSheet.classList.remove('open-full');
-        currentState = 'closed';
+        bottomSheet.classList.remove("open-full");
+        currentState = "closed";
         overlay.classList.remove("overlay--show");
       } else if (deltaY > -200 && deltaY < 0) {
-        bottomSheet.classList.remove('open-full');
-        bottomSheet.classList.add('open-half');
-        currentState = 'open-half';
+        bottomSheet.classList.remove("open-full");
+        bottomSheet.classList.add("open-half");
+        currentState = "open-half";
       }
     }
-  }
+  };
 
   const dragEnd = (e) => {
     if (!isDragging) return;
     isDragging = false;
     console.log("dragEnd");
-  }
+  };
 
   handleBtn.addEventListener("mousedown", dragStart);
   handleBtn.addEventListener("mousemove", dragging);
@@ -114,7 +114,7 @@ function initAside() {
   window.onclick = (e) => {
     if (isMobile()) {
       if (e.target.classList.contains("o-aside__toc-link")) {
-        bottomSheet.classList.remove('open-half');
+        bottomSheet.classList.remove("open-half");
         overlay.classList.remove("overlay--show");
       }
     }
